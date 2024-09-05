c0239a9fec75601443f2e3e2cd15f6680800ce42
ldh52 <yahopet79@gmail.com>
2024-09-05 12:50:21 +0900

    HTTP 데이터를 객체로 처리하는 방법

---
+++ src/main/java/com/sparta/springmvc/request/Star.java
@@ -0,0 +1,11 @@
+package com.sparta.springmvc.request;
+
+public class Star {
+    String name;
+    int age;
+
+    public Star(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
